327f6f161d746511534d365a754a41c8684eb969
Hydrargent <Yann>
2018-04-06 02:34:03 -0400

    Inputs + Menus WIP

---
+++ PixelChallenge18/Assets/Sources/ButtonsAnimEvents.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonsAnimEvents : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void PlayClicked ()
+    {
+        UIManager.s_Singleton.OnClickPlay();
+    }
+
+    public void CreditsClicked()
+    {
+        UIManager.s_Singleton.OnClickCredits();
+    }
+
+    public void ExitClicked()
+    {
+        UIManager.s_Singleton.OnClickExit();
+    }
+}
