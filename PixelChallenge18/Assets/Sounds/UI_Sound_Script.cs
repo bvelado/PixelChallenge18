80a983c6843aa2469c8dc79a57cc8fe454986b97
YoannYo <morvan.yoann@gmail.com>
2018-04-06 20:09:20 -0400

    UI sound script with functional events

---
+++ PixelChallenge18/Assets/Sounds/UI_Sound_Script.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Sound_Script : MonoBehaviour {
+
+public void OnHighlight ()
+    {
+        AkSoundEngine.PostEvent("Play_UI_Highlight", gameObject);
+    }
+
+
+    public void OnClicked()
+    {
+        AkSoundEngine.PostEvent("Play_UI_Click", gameObject);
+    }
+
+}
