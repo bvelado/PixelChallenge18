e13cf17745fc7524c52aaba0450fbea14ded3dd2
YoannYo <morvan.yoann@gmail.com>
2018-04-06 23:16:04 -0400

    created and added sound script to Player prefab

---
+++ PixelChallenge18/Assets/Sounds/Character_Sound_Script.cs
@@ -0,0 +1,12 @@
+﻿
+using UnityEngine;
+
+public class Character_Sound_Script : MonoBehaviour {
+
+
+    public void LaunchSoundEvent(string eventKey)
+    {
+        AkSoundEngine.PostEvent(eventKey, gameObject);
+    }
+	
+}
